2dfddf84adc7ca6c8d2a3d02d3b6854c36b5cea2
tertu marybig <flameshadowxeroshin@gmail.com>
2025-05-24 09:15:22 -0500

    Fix indentation in

--- rng-pmdred-iwram.s
+++ rng-pmdred-iwram.s
@@ -13,11 +13,11 @@ ExMTState: .dw 0,0,0,0
 
 DungRand32:
     ; The basic SFC32 random number generator;
-	push {r4}
-	ldr r12, =DungState
-	ldmia r12, {r1-r4}
+    push {r4}
+    ldr r12, =DungState
+    ldmia r12, {r1-r4}
     ; result = a + b + ctr++
-	add r0, r1, r2
+    add r0, r1, r2
     add r0, r0, r4
     add r4, r4, #1
     ; a = b ^ (b << 9)
@@ -27,7 +27,7 @@ DungRand32:
     ; c = result + rol(c, 21)
     add r3, r0, r3, ror #11
     stmia r12, {r1-r4}
-	pop {r4}
+    pop {r4}
     bx lr
 
 DungRand16:
@@ -45,28 +45,28 @@ DungRandInt:
     ; If the argument is 0 or negative, do it the old way.
     cmp r0, #0
     ble @@bad
-	push {r4-r6}
+    push {r4-r6}
     ; Calculate a bitmask for more efficient unbiased rejection sampling.
     ; This reduces the worst case repeat probability to 50% for any argument;
     ; usually it is better than that.
-	sub r6, r0, #1
-	orr r5, r6, r6, lsr #1
-	orr r5, r5, r5, lsr #2
-	orr r5, r5, r5, lsr #4
-	orr r5, r5, r5, lsr #8
-	orr r5, r5, r5, lsr #16
-	ldr r12, =DungState
-	ldmia r12, {r1-r4}
+    sub r6, r0, #1
+    orr r5, r6, r6, lsr #1
+    orr r5, r5, r5, lsr #2
+    orr r5, r5, r5, lsr #4
+    orr r5, r5, r5, lsr #8
+    orr r5, r5, r5, lsr #16
+    ldr r12, =DungState
+    ldmia r12, {r1-r4}
 @@loop:
-	add r0, r1, r2
+    add r0, r1, r2
     add r0, r0, r4
     add r4, r4, #1
     eor r1, r2, r2, lsr #9
     add r2, r3, r3, lsl #3
     add r3, r0, r3, ror #11
-	and r0, r0, r5
-	cmp r0, r6
-	bhi @@loop
+    and r0, r0, r5
+    cmp r0, r6
+    bhi @@loop
     stmia r12, {r1-r4}
     pop {r4-r6}
     bx lr
@@ -96,23 +96,23 @@ DungRandRange:
     sub r7, r1, r0
     sub r7, r7, #1
     orr r5, r7, r7, lsr #1
-	orr r5, r5, r5, lsr #2
-	orr r5, r5, r5, lsr #4
-	orr r5, r5, r5, lsr #8
-	orr r5, r5, r5, lsr #16
+    orr r5, r5, r5, lsr #2
+    orr r5, r5, r5, lsr #4
+    orr r5, r5, r5, lsr #8
+    orr r5, r5, r5, lsr #16
 
     ldmia r12, {r1-r4}
 @@loop:
     ; bitmasked rejection sampling, as above
-	add r6, r1, r2
+    add r6, r1, r2
     add r6, r6, r4
     add r4, r4, #1
     eor r1, r2, r2, lsr #9
     add r2, r3, r3, lsl #3
     add r3, r6, r3, ror #11
-	and r6, r6, r5
-	cmp r6, r7
-	bhi @@loop
+    and r6, r6, r5
+    cmp r6, r7
+    bhi @@loop
     stmia r12, {r1-r4}
     add r0, r0, r6
     pop {r4-r7}
@@ -125,12 +125,12 @@ DungRandRange:
 
 ; bool32 DungRandOutcome(s32 percentChance)
 DungRandOutcome:
-	push {r4-r8}
-	ldr r12, =DungState
-	mov r5, #100
-	ldmia r12, {r1-r4}
+    push {r4-r8}
+    ldr r12, =DungState
+    mov r5, #100
+    ldmia r12, {r1-r4}
 @@loop:
-	add r8, r1, r2
+    add r8, r1, r2
     add r8, r8, r4
     add r4, r4, #1
     eor r1, r2, r2, lsr #9
@@ -140,17 +140,17 @@ DungRandOutcome:
     ; This method has the advantage of a relatively low reroll probability,
     ; but requires the calculation of 2^32 % upperBound. As upperBound is a
     ; constant in this case, it's more efficient.
-	umull r6, r7, r8, r5
+    umull r6, r7, r8, r5
     ; did the low 32 bits indicate it's in the biased range?
-	cmp r6, #96
-	blo @@loop ; yes, try again
-	stmia r12, {r1-r4}
+    cmp r6, #96
+    blo @@loop ; yes, try again
+    stmia r12, {r1-r4}
     ; return true if r7 is less than the chance
-	cmp r7, r0
-	movlt r0, #1
+    cmp r7, r0
+    movlt r0, #1
     movge r0, #0
-	pop {r4-r8}
-	bx lr
+    pop {r4-r8}
+    bx lr
 
 gDungeon_Loc equ 0x203B418
 
@@ -161,23 +161,23 @@ DungRandInit:
     add r3, r3, #0x1C400 ;calculate the offset to the DungeonPosition
     add r3, r3, #0x170
     ldr r1, [r3]  ; load the DungeonPosition (type punning)
-	mov r2, r0
-	mov r3, r0
-	mov r4, #1
+    mov r2, r0
+    mov r3, r0
+    mov r4, #1
     ; do the mixing
 @@loop:
-	add r0, r1, r2
+    add r0, r1, r2
     add r0, r0, r4
     add r4, r4, #1
     eor r1, r2, r2, lsr #9
     add r2, r3, r3, lsl #3
     add r3, r0, r3, ror #11
-	cmp r4, #0x11
-	bne @@loop
-	ldr r12, =DungState
-	stmia r12, {r1-r4}
+    cmp r4, #0x11
+    bne @@loop
+    ldr r12, =DungState
+    stmia r12, {r1-r4}
     pop {r4}
-	bx lr
+    bx lr
 
 DungState: .dw 0, 0, 0, 0
 DungRandInitFlag: .dw 0
